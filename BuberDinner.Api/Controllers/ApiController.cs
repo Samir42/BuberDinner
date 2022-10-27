0b703a36fb61d93176526ad33758036fa5b7c82d
SamirOsmanov <samir.netdev@outlook.com>
2022-10-27 15:53:28 +0400

    first commit

---
+++ BuberDinner.Api/Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+using BuberDinner.Api.Common.Http;
+using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+public class ApiController : ControllerBase
+{
+    protected IActionResult Problem(List<Error> errors)
+    {
+        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+
+        var firstError = errors[0];
+
+        var statusCode = firstError.Type switch
+        {
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, detail: firstError.Description);
+    }
+}
